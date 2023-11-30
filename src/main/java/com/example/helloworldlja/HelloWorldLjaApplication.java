53d72f68824c6ab6a9db68bbf9c7346eb10e4574
JoseABermudez57 <jose.apep570@gmail.com>
2023-11-30 12:05:49 -0600

    first commit

---
+++ src/main/java/com/example/helloworldlja/HelloWorldLjaApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworldlja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldLjaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldLjaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/helloworldlja/HelloWorldLjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworldlja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldLjaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
